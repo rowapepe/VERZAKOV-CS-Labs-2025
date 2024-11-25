b56ce08312a22c84496bafa34fab9519890a63f1
rowapepe <nikver74@gmail.com>
2024-11-25 22:25:42 +0300

    fixed hpp

--- IntegralSolver.hpp
+++ IntegralSolver.hpp
@@ -2,7 +2,7 @@
 
 #include <cstddef>
 
-namespace {
+namespace IntegralSolver {
 struct I_print {
     const char* name;
     double i_sum;
@@ -14,9 +14,7 @@ struct Results {
     double sum;
     int n;
 };
-}  // namespace
 
-namespace IntegralSolver {
 Results IntRect(double (*function)(double), double a, double b, double eps);
 Results IntTrap(double (*function)(double), double a, double b, double eps);
 
