16c62a2b4b736f6620e587e77afbebacb424272a
rowapepe <nikver74@gmail.com>
2024-12-09 19:24:55 +0300

    save

--- InverseMatrixCalculator.hpp
+++ InverseMatrixCalculator.hpp
@@ -1,7 +1,14 @@
 #pragma once
 
 #include <cstddef>
+#include <fstream>
 
 namespace InverseMatrixCalculator {
+bool GaussJordan(double** matrix, int size);
+
+void MultiplyMatrix(double** matrixA, double** matrixB, double** result, int size);
+
+void FillMatrix(double** matrix, std::ifstream& file, int size);
+
 void ExecuteApp();
-}
+}  // namespace InverseMatrixCalculator
