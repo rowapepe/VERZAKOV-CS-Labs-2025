f4c0832def2d4be422852815d299b6e298db2162
rowapepe <nikver74@gmail.com>
2025-02-11 17:27:27 +0300

    approved

--- mymath/CMakeFiles/mymath.dir/mymath.cpp.o.d
+++
@@ -1,4 +0,0 @@
-mymath/CMakeFiles/mymath.dir/mymath.cpp.o: \
- /home/rowapepe/VERZAKOV-CS-Labs-2025/mymath/mymath.cpp \
- /usr/include/stdc-predef.h \
- /home/rowapepe/VERZAKOV-CS-Labs-2025/mymath/mymath.h
