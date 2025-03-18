98b6f750cf543b8fbe4ce3e8686e37ab0cd0e8d3
rowapepe <nikver74@gmail.com>
2025-03-18 17:14:40 +0300

    approved

--- containers/myset.h
+++ containers/myset.h
@@ -6,96 +6,131 @@
 
 template<class T>
 class MySet : public MyVector<T> {
- public:
-    MySet(T el = NULL) : MyVector<T>(el) {}
+public:
+    MySet(T el = NULL);
+    bool operator==(const MySet<T>& s) const;
 
-    bool operator==(const MySet<T>& s) const {
-        if (this->size != s.size) {
-            return false;
-        }
-        for (int i = 0; i < this->size; ++i) {
-            if (!s.is_element(this->operator[](i))) {
-                return false;
-            }
-        }
-        return true;
-    }
+    MySet<T>& operator+=(const MySet<T>& s);
+    MySet<T>& operator-=(const MySet<T>& s);
+    MySet<T>& operator*=(const MySet<T>& s);
 
-    MySet<T>& operator+=(const MySet<T>& s) {
-        for (int i = 0; i < s.size; ++i) {
-            this->add_element(s.operator[](i));
-        }
-        return *this;
-    }
+    void add_element(T el);
+    void delete_element(T el);
+    bool is_element(T el) const;
 
-    MySet<T>& operator-=(const MySet<T>& s) {
-        for (int i = 0; i < s.size; ++i) {
-            this->delete_element(s.operator[](i));
-        }
-        return *this;
-    }
+    template <typename U>
+    friend std::ostream& operator<<(std::ostream& out, const MySet<U>& s);
 
-    MySet<T>& operator*=(const MySet<T>& s) {
-        for (int i = 0; i < this->size; ++i) {
-            if (!s.is_element(this->operator[](i))) {
-                this->delete_element(this->operator[](i));
-            }
-        }
-        return *this;
-    }
+    template <typename U>
+    friend MySet<U> operator+(const MySet<U>& s1, const MySet<U>& s2);
 
-    void add_element(T el) {
-        if (!this->is_element(el)) {
-            MyVector<T>::add_element(el);
-            this->sort();
-        }
-    }
+    template <typename U>
+    friend MySet<U> operator-(const MySet<U>& s1, const MySet<U>& s2);
 
-    void delete_element(T el) {
-        int index = this->find(el);
-        if (index != -1) {
-            MyVector<T>::delete_element(index);
-        }
-    }
+    template <typename U>
+    friend MySet<U> operator*(const MySet<U>& s1, const MySet<U>& s2);
+};
 
-    bool is_element(T el) const {
-        for (int i = 0; i < this->size; ++i) {
-            if (this->operator[](i) == el) {
-                return true;
-            }
-        }
+template<class T>
+MySet<T>::MySet(T el) : MyVector<T>(el) {}
+
+template<class T>
+bool MySet<T>::operator==(const MySet<T>& s) const {
+    if (this->size != s.size) {
         return false;
     }
-
-    friend std::ostream& operator<<(std::ostream& out, const MySet<T>& s) {
-        out << "[";
-        for (int i = 0; i < s.size; ++i) {
-            if (i > 0) {
-                out << ", ";
-            }
-            out << s.operator[](i);
+    for (int i = 0; i < this->size; ++i) {
+        if (!s.is_element(this->operator[](i))) {
+            return false;
         }
-        out << "]";
-        return out;
     }
+    return true;
+}
 
-    friend MySet<T> operator+(const MySet<T>& s1, const MySet<T>& s2) {
-        MySet<T> result = s1;
-        result += s2;
-        return result;
+template<class T>
+MySet<T>& MySet<T>::operator+=(const MySet<T>& s) {
+    for (int i = 0; i < s.size; ++i) {
+        this->add_element(s[i]);
     }
+    return *this;
+}
 
-    friend MySet<T> operator-(const MySet<T>& s1, const MySet<T>& s2) {
-        MySet<T> result = s1;
-        result -= s2;
-        return result;
+template<class T>
+MySet<T>& MySet<T>::operator-=(const MySet<T>& s) {
+    for (int i = 0; i < s.size; ++i) {
+        this->delete_element(s[i]);
     }
+    return *this;
+}
 
-    friend MySet<T> operator*(const MySet<T>& s1, const MySet<T>& s2) {
-        MySet<T> result = s1;
-        result *= s2;
-        return result;
+template<class T>
+MySet<T>& MySet<T>::operator*=(const MySet<T>& s) {
+    for (int i = 0; i < this->size; ++i) {
+        if (!s.is_element(this->operator[](i))) {
+            this->delete_element(this->operator[](i));
+        }
     }
-};
+    return *this;
+}
+
+template<class T>
+void MySet<T>::add_element(T el) {
+    if (!this->is_element(el)) {
+        MyVector<T>::add_element(el);
+        this->sort();
+    }
+}
+
+template<class T>
+void MySet<T>::delete_element(T el) {
+    int index = this->find(el);
+    if (index != -1) {
+        MyVector<T>::delete_element(index);
+    }
+}
+
+template<class T>
+bool MySet<T>::is_element(T el) const {
+    for (int i = 0; i < this->size; ++i) {
+        if (this->operator[](i) == el) {
+            return true;
+        }
+    }
+    return false;
+}
+
+template<class U>
+std::ostream& operator<<(std::ostream& out, const MySet<U>& s) {
+    out << "[";
+    for (int i = 0; i < s.size; ++i) {
+        if (i > 0) {
+            out << ", ";
+        }
+        out << s[i];
+    }
+    out << "]";
+    return out;
+}
+
+template<class U>
+MySet<U> operator+(const MySet<U>& s1, const MySet<U>& s2) {
+    MySet<U> result = s1;
+    result += s2;
+    return result;
+}
+
+template<class U>
+MySet<U> operator-(const MySet<U>& s1, const MySet<U>& s2) {
+    MySet<U> result = s1;
+    result -= s2;
+    return result;
+}
+
+template<class U>
+MySet<U> operator*(const MySet<U>& s1, const MySet<U>& s2) {
+    MySet<U> result = s1;
+    result *= s2;
+    return result;
+}
 
 #endif  // INHERITANCE_MYSET_H
--- containers/myvector.h
+++ containers/myvector.h
@@ -8,137 +8,158 @@ const int MAX_SIZE = 5;
 
 template<class T>
 class MyVector {
- protected:
+protected:
     int max_size;
     int size;
     T* pdata;
-    void resize(bool increase) {
-        if (increase) {
-            max_size *= 2;
-            T* new_pdata = new T[max_size];
-            for (int i = 0; i < size; ++i) {
-                new_pdata[i] = pdata[i];
+    void resize(bool increase);
+
+public:
+    MyVector(T el = NULL, int maxsize = MAX_SIZE);
+    MyVector(const MyVector& v);
+    ~MyVector();
+
+    void add_element(T el);
+    bool delete_element(int i);
+    const T& operator[](int i) const;
+    void sort();
+    int find(T el) const;
+
+    MyVector<T>& operator=(const MyVector<T>& v);
+
+    template <typename U>
+    friend std::ostream& operator<<(std::ostream& out, const MyVector<U>& v);
+};
+
+template<class T>
+void MyVector<T>::resize(bool increase) {
+    if (increase) {
+        max_size *= 2;
+    } else {
+        max_size /= 2;
+    }
+    T* new_pdata = new T[max_size];
+    for (int i = 0; i < size; ++i) {
+        new_pdata[i] = pdata[i];
+    }
+    delete[] pdata;
+    pdata = new_pdata;
+}
+
+template<class T>
+MyVector<T>::MyVector(T el, int maxsize) : max_size(maxsize), size(0) {
+    pdata = new T[max_size];
+    if (el) {
+        add_element(el);
+    }
+}
+
+template<class T>
+MyVector<T>::MyVector(const MyVector& v) {
+    max_size = v.max_size;
+    size = v.size;
+    pdata = new T[max_size];
+    for (int i = 0; i < size; ++i) {
+        pdata[i] = v.pdata[i];
+    }
+}
+
+template<class T>
+MyVector<T>::~MyVector() {
+    delete[] pdata;
+}
+
+template<class T>
+void MyVector<T>::add_element(T el) {
+    if (!el) {
+        return;
+    }
+    if (size >= max_size) {
+        resize(true);
+    }
+    pdata[size] = el;
+    ++size;
+}
+
+template<class T>
+bool MyVector<T>::delete_element(int i) {
+    if (size == 0 || i < 0 || i >= size) {
+        return false;
+    }
+    for (int j = i; j < size - 1; ++j) {
+        pdata[j] = pdata[j + 1];
+    }
+    --size;
+
+    if (size > 0 && size <= max_size / 4 && max_size > MAX_SIZE) {
+        resize(false);
+    }
+    return true;
+}
+
+template<class T>
+const T& MyVector<T>::operator[](int i) const {
+    if (i < 0 || i >= size) {
+        throw std::invalid_argument("Индекс не валидный.");
+    }
+    return pdata[i];
+}
+
+template<class T>
+void MyVector<T>::sort() {
+    bool sorted = false;
+    for (int i = 0; i < size && !sorted; ++i) {
+        sorted = true;
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (pdata[j] > pdata[j + 1]) {
+                std::swap(pdata[j], pdata[j + 1]);
+                sorted = false;
             }
-            delete[] pdata;
-            pdata = new_pdata;
-        } else {
-            max_size /= 2;
-            T* new_pdata = new T[max_size];
-            for (int i = 0; i < size; ++i) {
-                new_pdata[i] = pdata[i];
-            }
-            delete[] pdata;
-            pdata = new_pdata;
         }
     }
+}
 
- public:
-    MyVector(T el = NULL, int maxsize = MAX_SIZE) : max_size(maxsize), size(0) {
-        pdata = new T[max_size];
-        if (el) {
-            add_element(el);
+template<class T>
+int MyVector<T>::find(T el) const {
+    for (int i = 0; i < size; ++i) {
+        if (pdata[i] == el) {
+            return i;
         }
     }
+    return -1;
+}
 
-    MyVector(const MyVector& v) {
-        max_size = v.max_size;
-        size = v.size;
-        pdata = new T[max_size];
-        for (int i = 0; i < size; ++i) {
-            pdata[i] = v.pdata[i];
-        }
-    }
-
-    ~MyVector() { delete[] pdata; }
-
-    void add_element(T el) {
-        if (!el) {
-            return;
-        }
-
-        if (size >= max_size) {
-            resize(true);
-        }
-        pdata[size] = el;
-        ++size;
-    }
-
-    bool delete_element(int i) {
-        if (size == 0 || i < 0 || i >= size) {
-            return false;
-        }
-        for (int j = i; j < size; ++j) {
-            pdata[j] = pdata[j + 1];
-        }
-        --size;
-
-        if (size > 0 && size <= max_size / 4 && max_size > MAX_SIZE) {
-            resize(false);
-        }
-
-        return true;
-    }
-
-    const T& operator[](int i) const {
-        if (i < 0 || i >= size) {
-            throw std::invalid_argument("Индекс не валидный.");
-        }
-        return pdata[i];
-    }
-
-    void sort() {
-        bool sorted = false;
-
-        for (int i = 0; i < size && !sorted; ++i) {
-            sorted = true;
-            for (int j = 0; j < size - i - 1; ++j) {
-                if (pdata[j] > pdata[j + 1]) {
-                    std::swap(pdata[j], pdata[j + 1]);
-                    sorted = false;
-                }
-            }
-        }
-    }
-
-    int find(T el) const {
-        for (int i = 0; i < size; ++i) {
-            if (pdata[i] == el) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    MyVector<T>& operator=(const MyVector<T>& v) {
-        delete[] pdata;
-
-        if (this == &v) {
-            return *this;
-        }
-
-        this->max_size = v.max_size;
-        this->size = v.size;
-        this->pdata = new T[max_size];
-        for (int i = 0; i < size; ++i) {
-            this->pdata[i] = v.pdata[i];
-        }
+template<class T>
+MyVector<T>& MyVector<T>::operator=(const MyVector<T>& v) {
+    if (this == &v) {
         return *this;
     }
 
-    friend std::ostream& operator<<(std::ostream& out, const MyVector<T>& v) {
-        for (int i = 0; i < v.size; ++i) {
-            out << v[i] << ' ';
-        }
-        out << '\n';
-        return out;
+    delete[] pdata;
+    max_size = v.max_size;
+    size = v.size;
+    pdata = new T[max_size];
+    for (int i = 0; i < size; ++i) {
+        pdata[i] = v.pdata[i];
     }
-};
+    return *this;
+}
+
+template<class U>
+std::ostream& operator<<(std::ostream& out, const MyVector<U>& v) {
+    for (int i = 0; i < v.size; ++i) {
+        out << v[i] << ' ';
+    }
+    out << '\n';
+    return out;
+}
+
 
 template<>
 void MyVector<const char*>::sort() {
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size - i - 1; ++j) {
-            if (!strcmp(pdata[j], pdata[j + 1])) {
+            if (strcmp(pdata[j], pdata[j + 1]) > 0) {
                 std::swap(pdata[j], pdata[j + 1]);
             }
         }
@@ -154,4 +175,5 @@ int MyVector<const char*>::find(const char* el) const {
     }
     return -1;
 }
+
 #endif  // INHERITANCE_MYVECTOR_H
